7df30db52c98f5cd439e59bd3dd9559ca47fe1f1
kimsunjung03 <sun3084aa@gmail.com>
2020-11-07 14:22:57 +0900

    update

--- src/main/java/com/example/springboard/service/posts/PostsService.java
+++ src/main/java/com/example/springboard/service/posts/PostsService.java
@@ -21,15 +21,16 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+    public Long update (Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
     }
